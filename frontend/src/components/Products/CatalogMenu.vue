1ef280f1dd90d1fae94aa2c939fc17cbf5d4f974
Clément Quaresma <clement.quaresma@gmail.com>
2019-04-10 10:27:33 -0400

    [feature] #7 - Reworked catalog menu

--- frontend/src/components/Products/CatalogMenu.vue
+++ frontend/src/components/Products/CatalogMenu.vue
@@ -1,28 +1,31 @@
 <template>
   <aside class="column is-one-fifth">
-    <p class="subtitle is-4">
+    <p class="subtitle is-4 has-text-centered">
       {{ department }}
     </p>
+    <hr class="navbar-divider" />
     <br />
     <p class="menu-label">
       Filter
     </p>
     <ul class="menu-list">
-      <b-dropdown aria-role="list">
-        <button class="button is-primary" slot="trigger">
-          <span>Per page</span>
-          <b-icon icon="caret-down"></b-icon>
-        </button>
-        <b-dropdown-item aria-role="listitem" @click="updatePerPage(10)"
-          >10</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" @click="updatePerPage(50)"
-          >50</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" @click="updatePerPage(100)"
-          >100</b-dropdown-item
-        >
-      </b-dropdown>
+      <a>
+        <b-dropdown aria-role="list">
+          <button class="button is-primary" slot="trigger">
+            <span>Per page</span>
+            <b-icon icon="caret-down"></b-icon>
+          </button>
+          <b-dropdown-item aria-role="listitem" @click="updatePerPage(10)"
+            >10</b-dropdown-item
+          >
+          <b-dropdown-item aria-role="listitem" @click="updatePerPage(50)"
+            >50</b-dropdown-item
+          >
+          <b-dropdown-item aria-role="listitem" @click="updatePerPage(100)"
+            >100</b-dropdown-item
+          >
+        </b-dropdown>
+      </a>
     </ul>
     <p class="menu-label">
       Avg. Customer Review
@@ -36,6 +39,7 @@
             :show-rating="false"
             :star-size="20"
           />
+          <p class="subtitle is-6">4 stars and above</p>
         </a>
       </li>
       <li @click="updateRating(3)">
@@ -46,6 +50,7 @@
             :show-rating="false"
             :star-size="20"
           />
+          <p class="subtitle is-6">3 stars and above</p>
         </a>
       </li>
       <li @click="updateRating(2)">
@@ -56,6 +61,7 @@
             :show-rating="false"
             :star-size="20"
           />
+          <p class="subtitle is-6">2 stars and above</p>
         </a>
       </li>
       <li @click="updateRating(1)">
@@ -66,6 +72,7 @@
             :show-rating="false"
             :star-size="20"
           />
+          <p class="subtitle is-6">1 star and above</p>
         </a>
       </li>
     </ul>
