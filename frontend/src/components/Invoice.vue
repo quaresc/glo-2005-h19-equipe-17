b1a9e94041db625e5d54d42b02f3421eb2f55a32
heraud_b <baptiste.heraud@epitech.eu>
2019-04-14 12:42:58 -0400

    View invoice

--- frontend/src/components/Invoice.vue
+++ frontend/src/components/Invoice.vue
@@ -12,21 +12,24 @@ export default {
       invoice: null,
       columns: [
         {
-          field: "id",
+          field: "id_invoice",
           label: "ID",
           width: "40",
           numeric: true
         },
         {
-          field: "at",
-          label: "date"
+          field: "montant",
+          label: "Somme total"
         }
       ]
     };
   },
+  created() {
+          this.id = this.$route.params.id;
+      },
   async mounted() {
     try {
-      this.invoice = await HTTP.get("/users/1/invoices").then(response => {
+      this.invoice = await HTTP.get("/users/"+ this.id + "/invoices").then(response => {
         return response.data;
       });
     } catch (error) {
